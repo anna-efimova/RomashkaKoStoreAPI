19bcbbbdd3b617c7b957dba695b88d1befc22a82
Anna Efi <AnyaFaceless@yandex.ru>
2024-07-18 13:54:22 +0300

    Implement the fourth task: adding filtering and sorting

--- src/main/java/com/example/romashkastoreapi/repository/ProductRepository.java
+++ src/main/java/com/example/romashkastoreapi/repository/ProductRepository.java
@@ -2,12 +2,26 @@ package com.example.romashkastoreapi.repository;
 
 
 import com.example.romashkastoreapi.model.ProductEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
+    @Query("select p from ProductEntity p where (:name is null or p.name like %:name%) " +
+        "and (:priceFrom is null or p.price >= :priceFrom ) " +
+        "and (:priceTo is null or p.price <= :priceTo) " +
+        "and (:inStock is null or p.inStock = :inStock) "
+    )
+    List<ProductEntity> getFilteredProducts(
+        String name, BigDecimal priceFrom, BigDecimal priceTo,
+        Boolean inStock, Pageable pageable
+    );
 }
 
 
